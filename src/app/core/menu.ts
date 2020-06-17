16baff887894e948254221f343e6afa6a5981e13
Brayan Carabali <brayanlc82@gmail.com>
2020-06-17 12:07:54 -0500

    feat(layout): I added the menu items

---
+++ src/app/core/menu.ts
@@ -0,0 +1,12 @@
+import {MenuModel} from './models/menu';
+
+export const menus: MenuModel[] = [
+  {
+    title: 'Proyectos',
+    path: 'projects',
+  },
+  {
+    title: 'Usuarios',
+    path: 'users',
+  },
+];

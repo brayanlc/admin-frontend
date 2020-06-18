4642645fe31a92b5f163a60fbfa9d8a60241820b
Brayan Carabali <brayanlc82@gmail.com>
2020-06-17 21:04:14 -0500

    feat(layout): I added the guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ContentLayoutComponent} from './layout/content-layout/content-layout.component';
 import {AuthLayoutComponent} from './layout/auth-layout/auth-layout.component';
+import {AuthGuard} from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: '',
     component: ContentLayoutComponent,
+    canActivateChild: [AuthGuard],
     children: [
       {
         path: '',
--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,17 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {UserListComponent} from "./pages/user-list/user-list.component";
-
+import {UserListComponent} from './pages/user-list/user-list.component';
+import {RolGuard} from '../../core/guards/rol.guard';
 
 const routes: Routes = [
   {
     path: 'users',
-    component: UserListComponent
-  }
+    component: UserListComponent,
+    canActivate: [RolGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
